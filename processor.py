449c2baf281384ccde8ec2df44250bcad2abf772
Fox Larsson <fox@artfox.dev>
2020-03-26 23:26:02 +0300

    Implemented stage 3/6
    Added multiplication by scalar
    Added matrix by matrix multiplication
    Added menu to choose operation

--- processor.py
+++ processor.py
@@ -1,24 +1,31 @@
-def build_matrix(rows):
-    matrix = []
-    for item in range(rows):
-        matrix.append([int(i) for i in input().split()])
-    return matrix
+def choose_action():
+    options = '''
+            1. Add matrices
+            2. Multiply matrix by a constant
+            3. Multiply matrices
+            0. Exit
+            Your choice: '''
+    return input(options)
 
 
 def get_num_rows_and_columns():
     return map(int, input().split())
 
 
-def construct_matrices():
-    num_rows_a, num_columns_a = get_num_rows_and_columns()
-    matrix_a = build_matrix(num_rows_a)
-    num_rows_b, num_columns_b = get_num_rows_and_columns()
-    matrix_b = build_matrix(num_rows_b)
-    return matrix_a, matrix_b
+def construct_matrix(num_rows, num_columns):
+    matrix = []
+    for row in range(num_rows):
+        matrix.append([float(i) for i in input().split()])
+    return matrix
 
 
-def check_matrix_alignment(matrix_a, matrix_b):
+def check_matrices_for_sum(matrix_a, matrix_b):
     if len(matrix_a) != len(matrix_b) or len(matrix_a[0]) != len(matrix_b[0]):
+        return False
+
+
+def check_matrices_for_multiplication(matrix_a, matrix_b):
+    if len(matrix_a[0]) != len(matrix_b):
         return False
 
 
@@ -30,18 +37,82 @@ def add_matrices(matrix_a, matrix_b):
     return matrix_sum
 
 
+def multiply_matrix_by_scalar(matrix):
+    scalar = float(input('Enter constant: '))
+    num_rows = len(matrix)
+    matrix_multiplied = []
+    for row in range(num_rows):
+        matrix_multiplied.append([item * scalar for item in matrix[row]])
+    return matrix_multiplied
+
+
+def multiply_matrices(matrix_a, matrix_b):
+    num_rows_a = len(matrix_a)
+    num_columns_a = len(matrix_a[0])
+    num_columns_b = len(matrix_b[0])
+    matrices_multiplied = []
+    for row in range(0, num_rows_a):
+        new_row = []
+        for column in range(0, num_columns_b):
+            dot_product = 0
+            for index in range(0, num_columns_a):
+                dot_product += matrix_a[row][index] * matrix_b[index][column]
+            new_row.append(dot_product)
+        matrices_multiplied.append(new_row)
+    return matrices_multiplied
+
+
 def print_matrix(matrix):
-    for row in range(len(matrix)):
-        print(*matrix[row])
+    print('The result is: ')
+    if matrix[0][0].is_integer():
+        for row in range(len(matrix)):
+            print(*(int(item) for item in matrix[row]), sep=' ')
+    else:
+        for row in range(len(matrix)):
+            print(*matrix[row])
 
 
 def main():
-    matrix_a, matrix_b = construct_matrices()
-    if check_matrix_alignment(matrix_a, matrix_b) is False:
-        print('ERROR')
-        exit()
-    matrix_sum = add_matrices(matrix_a, matrix_b)
-    print_matrix(matrix_sum)
+    while True:
+        action = choose_action()
+        # exit
+        if action == '0':
+            return False
+        # add matrices
+        elif action == '1':
+            rows_a, columns_a = map(int, input('Enter size of first matrix: ').split())
+            print('Enter first matrix: ')
+            matrix_a = construct_matrix(rows_a, columns_a)
+            rows_b, columns_b = map(int, input('Enter size of second matrix: ').split())
+            print('Enter second matrix: ')
+            matrix_b = construct_matrix(rows_b, columns_b)
+            if check_matrices_for_sum(matrix_a, matrix_b) is False:
+                print('ERROR: The operation cannot be performed: number of rows and columns in both matrices should '
+                      'match for addition')
+            else:
+                matrix_sum = add_matrices(matrix_a, matrix_b)
+                print_matrix(matrix_sum)
+        # multiply matrix by scalar
+        elif action == '2':
+            rows, columns = map(int, input('Enter size of matrix: ').split())
+            print('Enter matrix: ')
+            matrix = construct_matrix(rows, columns)
+            matrix_multiplied = multiply_matrix_by_scalar(matrix)
+            print_matrix(matrix_multiplied)
+        # multiply matrices
+        elif action == '3':
+            rows_a, columns_a = map(int, input('Enter size of first matrix: ').split())
+            print('Enter first matrix: ')
+            matrix_a = construct_matrix(rows_a, columns_a)
+            rows_b, columns_b = map(int, input('Enter size of second matrix: ').split())
+            print('Enter second matrix: ')
+            matrix_b = construct_matrix(rows_b, columns_b)
+            if check_matrices_for_multiplication(matrix_a, matrix_b) is False:
+                print('ERROR: the number of columns in matrix one must match the number of rows '
+                      'in matrix two for multiplication')
+            else:
+                matrices_multiplied = multiply_matrices(matrix_a, matrix_b)
+                print_matrix(matrices_multiplied)
 
 
 main()
